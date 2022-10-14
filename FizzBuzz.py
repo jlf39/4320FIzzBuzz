8bd0ffd8d9061805aa3efdfb2600a87ede2606c5
jlf39 <89615721+jlf39@users.noreply.github.com>
2022-10-14 11:37:42 -0500

    print fizz or buzz if number divisible by 3 or 5

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,6 +1,12 @@
 def Count():
     x=1
     while x<101:
-        print(x)
+        printforb(x)
         x=x+1
+
+def printforb(x):
+    if x%3 == 0: print("Fizz")
+    if x%5 == 0: print("Buzz")
+    if x%3 !=0 and x%5 !=0: print(x)
+
 Count()
